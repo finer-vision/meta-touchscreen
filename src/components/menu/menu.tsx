3390184d5037dbe946ad2f506c2295a2e258436f
Moses <mosesaizee@outlook.com>
2022-09-08 13:52:58 +0100

    wip: new menu component

---
+++ src/components/menu/menu.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { MenuWrapper } from "@/components/menu/menu.styles";
+
+export default function Menu() {
+  return (
+      <MenuWrapper>
+
+      </MenuWrapper>
+  );
+}

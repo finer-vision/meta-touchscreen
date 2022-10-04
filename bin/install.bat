4cf0f3c9b804512d3787a2e8898988b6fed7e98f
Enijar <TheEnijar@gmail.com>
2022-10-04 09:21:29 +0100

    fix: install

--- bin/install.bat
+++ bin/install.bat
@@ -1,1 +1,1 @@
-cp %USERPROFILE%\meta-touchscreen\bin\touchscreen.bat "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+copy %USERPROFILE%\meta-touchscreen\bin\touchscreen.bat "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"

c64858dcc48ed4dd5f4be26798e099577b2f629a
Enijar <TheEnijar@gmail.com>
2023-09-19 09:16:43 +0100

    chore: remove unused util file

--- src/utils.ts
+++
@@ -1,6 +0,0 @@
-export function isLocal(): boolean {
-  const hostname = window.location.hostname;
-  if (hostname === "localhost") return true;
-  if (hostname === "0.0.0.0") return true;
-  return hostname.startsWith("192.168.");
-}

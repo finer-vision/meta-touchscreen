beca1fbfbb4327059523d3da8b8aea7024f14330
Enijar <TheEnijar@gmail.com>
2024-04-23 11:36:37 +0100

    chore: update dependencies

--- src/components/menu/menu.styles.ts
+++ src/components/menu/menu.styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components/macro";
+import styled, { css } from "styled-components";
 import { motion } from "framer-motion";
 
 export const MenuWrapper = styled.div`
--- src/styles/reset.ts
+++ src/styles/reset.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components/macro";
+import { createGlobalStyle } from "styled-components";
 import { fluidRange } from "polished";
 import vars from "./vars";
 

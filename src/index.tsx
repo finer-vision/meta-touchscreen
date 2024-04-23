da9d7483b86a65f37434b862c2dab9834394450d
Enijar <TheEnijar@gmail.com>
2024-04-23 12:58:42 +0100

    chore: remove analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter as Router } from "react-router-dom";
-import { AnalyticsProvider } from "fv-analytics";
 import App from "@/components/app/app";
 import Reset from "@/styles/reset";
 
@@ -9,16 +8,7 @@ const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
   <Router>
-    <AnalyticsProvider
-      config={{
-        serverUrl: "http://localhost:3000",
-        storageKey: "meta-touchscreen-analytics",
-        deviceId: "meta-touchscreen",
-        projectId: "meta-touchscreen",
-      }}
-    >
       <Reset />
       <App />
-    </AnalyticsProvider>
   </Router>,
 );

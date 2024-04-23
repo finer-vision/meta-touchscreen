9b76f8c1b8b558826e94803f918c95384e73a4f7
Enijar <TheEnijar@gmail.com>
2024-04-23 12:17:47 +0100

    wip: integrate test fv-analytics package

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter as Router } from "react-router-dom";
+import { AnalyticsProvider } from "fv-analytics";
 import App from "@/components/app/app";
 import Reset from "@/styles/reset";
 
@@ -8,7 +9,16 @@ const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
   <Router>
-    <Reset />
-    <App />
-  </Router>
+    <AnalyticsProvider
+      config={{
+        serverUrl: "http://localhost:3000",
+        storageKey: "meta-touchscreen-analytics",
+        deviceId: "meta-touchscreen",
+        projectId: "meta-touchscreen",
+      }}
+    >
+      <Reset />
+      <App />
+    </AnalyticsProvider>
+  </Router>,
 );
